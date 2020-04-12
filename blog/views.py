993c30fc4ac0b6838d97cdd738ca8d4e6b7e2b9b
Guilherme Sartori <guimsartori@gmail.com>
2020-04-12 19:18:09 -0300

    Added pagination and filtering by user

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
 	ListView, 
 	DetailView, 
@@ -7,7 +7,9 @@ from django.views.generic import (
 	DeleteView
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from .models import Post
+
 
 def home(request):
 	context = {
@@ -22,6 +24,19 @@ class PostListView(ListView):
 	template_name = 'blog/home.html'
 	context_object_name = 'posts'
 	ordering = ['-date_posted']
+	paginate_by = 5
+
+
+class UserPostListView(ListView):
+	model = Post
+	template_name = 'blog/user_posts.html'
+	context_object_name = 'posts'
+	paginate_by = 5
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get("username"))
+		return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
 	model = Post
@@ -34,6 +49,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 	def form_valid(self, form):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Post
